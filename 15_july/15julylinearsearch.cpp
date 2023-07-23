6bda67d7542f418238a82b216cba6463c32092e1
Rohan12306105 <138663681+Rohan12306105@users.noreply.github.com>
2023-07-23 16:42:03 +0530

    Add files via upload

---
+++ 15_july/15julylinearsearch.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int a[5] = {1, 2, 3, 4, 5};
+	int n = sizeof(a) / sizeof(int);
+
+	int key = 3, i;
+
+	for (i = 0; i < n; ++i)
+	{
+		if (a[i] == key) {
+			cout << "Key Found: " << i << endl;
+			break;
+		}
+	}
+
+	if (i == n) {
+		cout << "Not Found\n";
+	}
+
+
+	return 0;
+}
